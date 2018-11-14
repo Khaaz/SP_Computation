1568d3f7c242f6572308f07e4868c8b7496bfb62
doauboueix <doauboueix@iutclinfc2101l.iut.local>
2018-11-14 17:36:36 +0100

    tp1 #2

--- tp1/tp.py
+++ tp1/tp.py
@@ -1,4 +1,11 @@
-def binpacking1(t, nbr, capacity):
+import random
+import time
+import math
+
+def primaryBinpacking(t, nbr, capacity):
+	minBox = math.ceil(nbr / capacity)
+
+def binpacking(t, nbr, capacity):
 	bins = []
 	
 	while t:
@@ -20,6 +27,46 @@ def binpacking1(t, nbr, capacity):
 		bins.append(box)
 	return bins
 
+def optiBinpacking(bins, capacity, startTime):
+	i = 0
+	while time.time() - startTime < 60:
+		randIndex = random.randrange(len(bins))
+		
+		cur = bins.pop(randIndex)
+		cur.sort(reverse=True)
+
+		for b in bins:
+			for elem in cur:
+				if sum(b) + elem <= capacity:
+					b.append(elem)
+					cur.remove(elem)
+
+		if cur:
+			bins.append(cur)
+
+		i += 1
+	return bins
+
+def optiBinpackCleaned(bins, capacity, startTime):
+	# Remove all box already full
+	tempBins = []
+	while sum(bins[0]) == capacity:
+		tempBins.append(bins.pop(0))
+
+	#
+	bins = optiBinpacking(bins, capacity, startTime)
+
+	# sync bins with tempBins
+	for b in tempBins:
+		bins.append(b)
+
+	return bins
+
+# setup
+startTime = time.time()
+random.seed(123456789)
+
+# Read stdin
 nbr = int(input())
 capacity = int(input())
 
@@ -28,7 +75,18 @@ t = [int(input()) for i in range(nbr)]
 
 # sort list
 t.sort(reverse=True)
-bins = binpacking1(t, nbr, capacity)
 
+#bins = primaryBinpacking()
+bins = binpacking(t, nbr, capacity)
+
+bins.sort(key=sum, reverse=True)
+if nbr < 1000:
+	bins = optiBinpackCleaned(bins, capacity, startTime)
+else:
+	bins = optiBinpacking(bins, capacity, startTime)
+
+# Print
 print(len(bins))
-#print(bins)
+for b in bins:
+	[print(e) for e in b]
+	print(" ")
