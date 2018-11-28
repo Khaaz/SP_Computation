db1dfaba010fa226e732e387fe99cbd301ad0b31
lobellec <lobellec@iutclinfc2101l.iut.local>
2018-11-28 17:33:24 +0100

    tp#3 convex hull

--- tp3/tp.py
+++ tp3/tp.py
@@ -1,4 +1,68 @@
 import random
+import math
+
+
+# 20 -> 1 min
+# 100 -> 1 min
+# 1000 -> 10 min
+# 10000 -> 10 min
+
+#Brute force:
+'''
+add nearest point over and over from the last poit
+'''
+
+# triangle:
+''' 
+pick 3 random point to create a triangle
+find nearest point, add point
+'''
+
+# convexe:
+'''
+create convex hull
+add nearest point (inside)
+'''
+
+# get nearest distance
+'''
+  C
+A  B
+
+A->C + C->B - A-B
+'''
+
+# convex hull from tuple
+def chT(v):
+	def ccw(p,q,r):
+		x = ((q[0]-p[0]) * (r[1]-p[1])) - ((r[0]-p[0]) * (q[1]-p[1]))
+		return x > 0 # True if p,q,r are oriented counterclockwise
+	def halfch(v):
+		hull = [v[0]]
+		for p in v[1:]:
+			while len(hull)>=2 and ccw(p, hull[-1], hull[-2]):
+				hull.pop()
+			hull.append(p)
+		return hull
+	top = halfch(sorted(v))
+	bottom = halfch(sorted(v, reverse=True))
+	return top[0:-1] + bottom[0:-1]
+
+# convex hull from Point
+def chP(v):
+	def ccw(p,q,r):
+		x = ((q.x-p.x) * (r.y-p.y)) - ((r.x-p.x) * (q.y-p.y))
+		return x > 0 # True if p,q,r are oriented counterclockwise
+	def halfch(v):
+		hull = [v[0]]
+		for p in v[1:]:
+			while len(hull)>=2 and ccw(p, hull[-1], hull[-2]):
+				hull.pop()
+			hull.append(p)
+		return hull
+	top = halfch(sorted(v, key=lambda p: p.x))
+	bottom = halfch(sorted(v, key=lambda p: p.x, reverse=True))
+	return top[0:-1] + bottom[0:-1]
 
 class Point:
 	x = 0
@@ -17,7 +81,7 @@ class Point:
 	def __repr__(self):
 		return "(" + str(self.x) + "," + str(self.y) + ")"
 
-	def nextNear(base):
+	def nextNear(self, base):
 		nearDist = 0
 		nearP = 0
 		for p in base:
@@ -35,62 +99,45 @@ class Point:
 		return [nearP]
 
 
-def buildNextList(base, p):
-	pNext = p.nextNear(base)
-	if len(pNext) == 0:
-		return
-	return pNext + buildNextList(base, p)
+def getDist(A, B, C):
+	return A.dist(C) + C.dist(B) - A.dist(B)
 
-def nextNearSearch(base):
-	randIndex = random.randrange(len(base))
-	cur = base[randIndex]
-	cur.mark = True
-	return [cur] + buildNextList(base, cur)
+# base = all points NOT in current hull
+def getSmallerDist(A, B, base):
+	smallerDist = 0;
+	nearP = 0
+	for e in base:
+		dist = getDist(A, B, e)
+		if smallerDist == 0 or dist < smallerDist:
+			smallerDist = dist
+			nearP = e
 
-def distFullRoute(base):
-	fullRoute = base
-	first = fullRoute.pop(0)
-	fullDist = sumDist(base, first, fullRoute[1])
-	return fullDist + first.dist(base[len(base)])
+	if nearP == 0:
+		return ()
+	else:
+		return (smallerDist, nearP)
 
-def sumDist(base, p, curP):
-	if len(base) == 0:
-		return 0
-	nextP = base.pop(0)
-	return p.dist(curP) + sumDist(base, curP, nextP)
+# create dict that associate pair of point with nearest point
+def createAllNeighbours(hull, free):
+	neighbours = dict()
+	for index in range(len(hull) - 1):
+		neighbours[(hull[index], hull[index + 1])] = createNeighbour(hull[index], hull[index + 1], free)
 
+	neighbours[(hull[0], hull[len(hull) - 1])] = createNeighbour(hull[0], hull[len(hull) - 1], free)
+	return neighbours
 
-# convex hull from tuple
-def ch(v):
-	def ccw(p,q,r):
-		x = ((q[0]-p[0]) * (r[1]-p[1])) - ((r[0]-p[0]) * (q[1]-p[1]))
-		return x > 0 # True if p,q,r are oriented counterclockwise
-	def halfch(v):
-		hull = [v[0]]
-		for p in v[1:]:
-			while len(hull)>=2 and ccw(p, hull[-1], hull[-2]):
-				hull.pop()
-			hull.append(p)
-		return hull
-	top = halfch(sorted(v))
-	bottom = halfch(sorted(v, reverse=True))
-	return top[0:-1] + bottom[0:-1]
+def createNeighbour(A, B, free):
+	return getSmallerDist(A, B, free)
 
-# convex hull from Point
-def ch(v):
-	def ccw(p,q,r):
-		x = ((q.x-p.x) * (r.y-p.y)) - ((r.x-p.x) * (q.y-p.y))
-		return x > 0 # True if p,q,r are oriented counterclockwise
-	def halfch(v):
-		hull = [v[0]]
-		for p in v[1:]:
-			while len(hull)>=2 and ccw(p, hull[-1], hull[-2]):
-				hull.pop()
-			hull.append(p)
-		return hull
-	top = halfch(sorted(v, key=lambda p: p.x))
-	bottom = halfch(sorted(v, key=lambda p: p.x, reverse=True))
-	return top[0:-1] + bottom[0:-1]
+def addPoint(hull):
+	# parcours dict
+	# find lowest dist
+	# ajoute point
+	# supprime old association of dict
+	# ajoute 2 nouvelles association
+	# check if another el in dict had this point
+	# if yes recalculate
+	1 + 1
 
 # Read stdin
 nbr = int(input())
@@ -100,10 +147,41 @@ base = [Point(input().split(' ')) for i in range(nbr)]
 # create list of tuple
 #base = [tuple(float(i) for i in input().split(' ')) for i in range(nbr)]
 
+hull = chP(base)
+# free = [ x for x in base if x not in hull ]
+free = set(base) - set(hull)
+
+neighbours = createAllNeighbours(hull, free)
+
+print(neighbours)
+# Print
+print(nbr)
 print(base)
+#[print(e) for e in final]
 
-final = nextNearSearch(base)
-fulldist = distFullRoute(base)
 
-print(final)
-print(fulldist)
+
+
+# def buildNextList(base, p):
+# 	pNext = p.nextNear(base)
+# 	if len(pNext) == 0:
+# 		return []
+# 	return pNext + buildNextList(base, p)
+
+# def nextNearSearch(base):
+# 	randIndex = random.randrange(len(base))
+# 	cur = base[randIndex]
+# 	cur.mark = True
+# 	return [cur] + buildNextList(base, cur)
+
+# def distFullRoute(base):
+# 	fullRoute = base[:]
+# 	first = fullRoute.pop(0)
+# 	fullDist = sumDist(fullRoute, first, fullRoute[1])
+# 	return fullDist + first.dist(base[len(base)-1])
+
+# def sumDist(base, p, curP):
+# 	if len(base) == 0:
+# 		return 0
+# 	nextP = base.pop(0)
+# 	return p.dist(curP) + sumDist(base, curP, nextP)
